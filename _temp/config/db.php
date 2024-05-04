7315188982e149ba5f2dabb0f00356ded7b4233f
Nvtan <nguyenvantyan@gmail.com>
2024-05-04 16:17:23 +0700

    update

---
+++ _temp/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=qlvt_3',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
